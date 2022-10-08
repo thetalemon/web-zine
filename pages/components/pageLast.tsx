193e9890f0e43bfbece153e1e58405a547e294b8
thetalemon <sasami0314@gmail.com>
2022-10-09 01:41:51 +0900

    mod: add noreferrer

--- pages/components/pageLast.tsx
+++ pages/components/pageLast.tsx
@@ -7,7 +7,7 @@ const PageLast:React.FC = () => {
     <div className={`${pageStyles.page} ${pageLastStyles.pageLast}`}>
       <p>少女たちと廃墟。</p>
       <p>画像生成： Stable Diffusion</p>
-      <p>加筆・サイト制作： <a href="https://manasas.dev/" target='_blank'>manasas</a></p>
+      <p>加筆・サイト制作： <a href="https://manasas.dev/" target='_blank' rel="noreferrer">manasas</a></p>
     </div>
   )
 }
